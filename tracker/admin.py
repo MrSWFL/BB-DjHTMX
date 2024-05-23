c404a256428622157bea4cbfc29df4993a7d9f78
MR <mlrwhitehall@gmail.com>
2024-05-22 21:00:19 -0400

    Start of Video 2

---
+++ tracker/admin.py
@@ -0,0 +1,8 @@
+# tracker/admin.py
+from django.contrib import admin
+from tracker.models import Category, Transaction
+
+# Register your models here.
+admin.site.register(Category)
+admin.site.register(Transaction)
+
